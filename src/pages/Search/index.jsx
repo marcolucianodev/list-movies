3cbafcbdeaeea7d737176aafc6079d2ba68131ba
marcolucianodev <maurelio.sluciano@gmail.com>
2023-01-07 01:29:24 -0300

    Fix fetch error

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import { MovieBox, MoviesContent, SearchMovieContainer } from "./styles";
-
-import { searchFetch } from "../../components/Tmdb/SearchData";
 import Loader from "../../components/Loader";
 
 
@@ -13,14 +11,22 @@ const Search = () => {
     const [searchParams] = useSearchParams()
     const query = searchParams.get("q")
 
+    const API_KEY = '75a925ab7a227e572033e0d7fcc3b9d3'
+    const BASE_URL = "https://api.themoviedb.org/3/search/movie/"
+
     useEffect(() => {      
 
-        const searchAllMovies = async () => {
-            const list = await searchFetch(query)
-            setSearchMovies(list)
+        const getSearchMovies = async () => {
+            try {
+                const response = await fetch(`${BASE_URL}?&language=pt-BR&api_key=${API_KEY}&query=${query}`)
+                const data = await response.json()
+                setSearchMovies(data.results)
+            } catch (error) {
+                console.log(error)
+            }
         }
 
-        searchAllMovies()
+        getSearchMovies();
 
     }, [query])
 
